346a5cb1add1e96e21ff5931dc7e96abbeeeff05
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2015-09-30 10:38:58 +0100

    It works, my pathmachine works!

--- par-shell.c
+++ par-shell.c
@@ -4,15 +4,17 @@
 #include "commandlinereader.h"
 #include "list.h"
 
-int main(){
-	char* argv[6]; argv[0] = NULL;
+#define PATHNAME_MAX_ARGS 5
+
+int main(int argc, char* argv[]){
+	char* argvec[PATHNAME_MAX_ARGS+1]; argvec[0] = NULL;
 	int read = 0, i;
-	read = readLineArguments(argv, 5);
+	read = readLineArguments(argvec, PATHNAME_MAX_ARGS+1);
 
 	printf("Number of read arguments: %d\n", read);
 
 	for(i=0; i<read; i++)
-		printf("%s\n", argv[i]);
+			printf("%s\n", argvec[i]);
 
 	return 0;
 }
